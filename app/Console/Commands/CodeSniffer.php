9077f344a512c49050e4c571a153e07f64b3fece
NauxLiu <nauxliu@gmail.com>
2015-09-22 07:36:46 +0800

    add CodeSniffer

---
+++ app/Console/Commands/CodeSniffer.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Symfony\Component\Process\Process;
+
+class CodeSniffer extends Command implements SelfHandling
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cbf:all';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '格式化代码';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $process = new Process('php-cs-fixer fix');
+
+        try {
+            $process->run(function ($type, $buffer) {
+                if (Process::ERR === $type) {
+                    $this->error($buffer);
+                } else {
+                    $this->info($buffer);
+                }
+            });
+
+            $this->line('Finished.');
+        } catch (Exception $e) {
+            $this->error($e->getMessage());
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CodeSniffer;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-
+        CodeSniffer::class,
     ];
 
     /**
@@ -23,6 +24,5 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-
     }
 }
