244b74ca97e8977584d8fb12635e6a344b530ac5
NauxLiu <nauxliu@gmail.com>
2015-09-22 20:52:31 +0800

    client belongs to user

--- database/migrations/2014_04_24_110459_create_oauth_clients_table.php
+++ database/migrations/2014_04_24_110459_create_oauth_clients_table.php
@@ -29,6 +29,7 @@ class CreateOauthClientsTable extends Migration
             $table->string('id', 40)->primary();
             $table->string('secret', 40);
             $table->string('name');
+            $table->unsignedInteger('user_id');
             $table->timestamps();
 
             $table->unique(['id', 'secret']);
