b82863e251b1a89bc219a15774bce2cfef505aca
NauxLiu <nauxliu@gmail.com>
2015-09-22 08:14:46 +0800

    Add BaseCriteria

--- app/Repositories/Criteria/BaseCriteria.php
+++ app/Repositories/Criteria/BaseCriteria.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories\Criteria;
 
+use Prettus\Repository\Contracts\RepositoryInterface;
 use Prettus\Repository\Criteria\RequestCriteria;
 
 /**
@@ -12,5 +13,79 @@ use Prettus\Repository\Criteria\RequestCriteria;
  */
 abstract class BaseCriteria extends RequestCriteria
 {
+    public function apply($model, RepositoryInterface $repository)
+    {
+        $fieldsSearchable   = $repository->getFieldsSearchable();
+        $search             = $this->request->get( config('repository.criteria.params.search','search') , null);
+        $searchFields       = $this->request->get( config('repository.criteria.params.searchFields','searchFields') , null);
+        $filter             = $this->request->get( config('repository.criteria.params.filter','filter') , null);
+        $orderBy            = $this->request->get( config('repository.criteria.params.orderBy','orderBy') , null);
+        $sortedBy           = $this->request->get( config('repository.criteria.params.sortedBy','sortedBy') , 'asc');
+        $with               = $this->request->get( config('repository.criteria.params.with','with') , null);
+        $sortedBy           = !empty($sortedBy) ? $sortedBy : 'asc';
+
+        if ( $search && is_array($fieldsSearchable) && count($fieldsSearchable) )
+        {
+
+            $searchFields       = is_array($searchFields) || is_null($searchFields) ? $searchFields : explode(';',$searchFields);
+            $fields             = $this->parserFieldsSearch($fieldsSearchable, $searchFields);
+            $isFirstField       = true;
+            $searchData         = $this->parserSearchData($search);
+            $search             = $this->parserSearchValue($search);
+            $modelForceAndWhere = false;
+
+            $model = $model->where(function ($query) use($fields, $search, $searchData, $isFirstField, $modelForceAndWhere) {
+                foreach ($fields as $field=>$condition) {
+
+                    if (is_numeric($field)){
+                        $field = $condition;
+                        $condition = "=";
+                    }
+
+                    $value = null;
+
+                    $condition  = trim(strtolower($condition));
+
+                    if ( isset($searchData[$field]) ) {
+                        $value = $condition == "like" ? "%{$searchData[$field]}%" : $searchData[$field];
+                    } else {
+                        if ( !is_null($search) ) {
+                            $value = $condition == "like" ? "%{$search}%" : $search;
+                        }
+                    }
+
+                    if ( $isFirstField || $modelForceAndWhere ) {
+                        if (!is_null($value)) {
+                            $query->where($field,$condition,$value);
+                            $isFirstField = false;
+                        }
+                    } else {
+                        if (!is_null($value)) {
+                            $query->orWhere($field,$condition,$value);
+                        }
+                    }
+                }
+            });
+        }
+
+        if ( isset($orderBy) && in_array(strtolower($orderBy), ['asc', 'desc']) ) {
+            $model = $model->orderBy($orderBy, $sortedBy);
+        }
+
+        if ( isset($filter) && !empty($filter) ) {
+            if ( is_string($filter) ) {
+                $filter = explode(';', $filter);
+            }
+
+            $model = $model->select($filter);
+        }
+
+        if( $with ) {
+            $with  = explode(';', $with);
+            $model = $model->with($with);
+        }
+
+        return $model;
+    }
 
 }
