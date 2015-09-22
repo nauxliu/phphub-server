398d7bfbfee0253aed2b7df7c1adc438a29dd8c3
NauxLiu <nauxliu@gmail.com>
2015-09-22 08:51:06 +0800

    improve TopicCriteria

--- app/Repositories/Criteria/TopicCriteria.php
+++ app/Repositories/Criteria/TopicCriteria.php
@@ -11,5 +11,53 @@ namespace App\Repositories\Criteria;
 
 class TopicCriteria extends BaseCriteria
 {
+    /**
+     * 精华帖
+     *
+     * @param $model
+     */
+    public function filterExcellent($model)
+    {
+        $model->where('is_excellent', 1);
+    }
 
+    /**
+     * Wiki 帖
+     *
+     * @param $model
+     */
+    public function filterWiki($model)
+    {
+        $model->where('is_wiki', 1);
+    }
+
+    /**
+     * 最新发表的贴
+     *
+     * @param $model
+     */
+    public function filterRecent($model)
+    {
+        $model->orderBy('created_at', 'desc');
+    }
+
+    /**
+     * 按照投票数倒序排序
+     *
+     * @param $model
+     */
+    public function filterVote($model)
+    {
+        $model->orderBy('vote_count', 'desc');
+    }
+
+    /**
+     * 无人回复的贴
+     *
+     * @param $model
+     */
+    public function filterNobody($model)
+    {
+        $model->where('reply_count', 0);
+    }
 }
