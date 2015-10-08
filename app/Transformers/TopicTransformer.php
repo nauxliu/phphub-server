bbb6b6b8f4b154b1a0af20567e6c210ee14a0820
NauxLiu <nauxliu@gmail.com>
2015-10-08 15:43:57 +0800

    improve TopicTransformer

--- app/Transformers/TopicTransformer.php
+++ app/Transformers/TopicTransformer.php
@@ -35,23 +35,12 @@ class TopicTransformer extends TransformerAbstract
      */
     public function transformData($model)
     {
-        return $model->toArray();
-
-        return [
-            'id'             => (int) $model->id,
-            'title'          => $model->title,
-            'body'           => $model->body,
-            'user_id'        => (int) $model->user_id,
-            'node_id'        => (int) $model->node_id,
-            'is_excellent'   => (boolean) $model->is_excellent,
-            'is_wiki'        => (boolean) $model->is_wiki,
-            'view_count'     => (int) $model->view_count,
-            'reply_count'    => (int) $model->reply_count,
-            'favorite_count' => (int) $model->favorite_count,
-            'vote_count'     => (int) $model->vote_count,
-            'created_at'     => $model->created_at,
-            'updated_at'     => $model->updated_at,
+        $data          = $model->toArray();
+        $data['links'] = [
+            'details_web_view' => route('topic.web_view', $model->id),
         ];
+
+        return $data;
     }
 
     public function includeUser($model)
