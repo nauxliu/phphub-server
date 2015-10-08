aeab039d05660bf0142080bf4f187efc616041a9
NauxLiu <nauxliu@gmail.com>
2015-10-08 15:44:15 +0800

    CS.

--- app/Helpers.php
+++ app/Helpers.php
@@ -37,13 +37,16 @@ function cdn($file_path)
 }
 
 /**
- * 获取 elixir 的 CDN 连接
+ * 获取 elixir 的 CDN 连接.
+ *
  * @param $file
+ *
  * @return string
  */
 function cdn_elixir($file)
 {
     $cdn_url = config('app.static_mirror_url');
+
     return $cdn_url ? trim($cdn_url, '/').elixir($file) : elixir($file);
 }
 
