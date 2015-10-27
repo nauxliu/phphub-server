0371afe281447d4f511a317f4eeaae46923a40c4
NauxLiu <nauxliu@gmail.com>
2015-10-27 15:11:14 +0800

    点击图片时通过 jsBridge 传递图片 URL

--- resources/views/api_web_views/topic.blade.php
+++ resources/views/api_web_views/topic.blade.php
@@ -6,14 +6,43 @@
     <link rel="stylesheet" href="{!! cdn_elixir('css/api.css') !!}">
 </head>
 <body>
-    <div class="header-title">
-        <h3>{!! $topic->title !!}</h3>
-        <p>{!! $topic->created_at->format('Y-m-d H:i:s') !!}  • {!! $topic->view_count !!} 阅读</p>
-    </div>
-    <div class="markdown-content">
-        {!! $topic->body !!}
-    </div>
+<div class="header-title">
+    <h3>{!! $topic->title !!}</h3>
 
-    <script src="{!! cdn_elixir('js/api.js') !!}"></script>
+    <p>{!! $topic->created_at->format('Y-m-d H:i:s') !!} • {!! $topic->view_count !!} 阅读</p>
+</div>
+<div class="markdown-content">
+    {!! $topic->body !!}
+</div>
+
+<script src="{!! cdn_elixir('js/api.js') !!}"></script>
+<script>
+    function connectWebViewJavascriptBridge(callback) {
+        if (window.WebViewJavascriptBridge) {
+            callback(WebViewJavascriptBridge)
+        } else {
+            document.addEventListener('WebViewJavascriptBridgeReady', function () {
+                callback(WebViewJavascriptBridge)
+            }, false)
+        }
+    }
+
+    connectWebViewJavascriptBridge(function (bridge) {
+        bridge.init(function (message, responseCallback) {
+            responseCallback(data)
+        });
+
+        var imgs = document.getElementsByTagName('img');
+
+        for (var i = 0; i < imgs.length; i++) {
+            imgs[i].onclick = function (e) {
+                var data = {"imageUrl": this.src};
+                bridge.send(data, function (responseData) {
+
+                })
+            }
+        }
+    })
+</script>
 </body>
 </html>
