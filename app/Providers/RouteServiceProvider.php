a4d1326e783b713c18f8cb602deb63837c02f5b4
NauxLiu <nauxliu@gmail.com>
2015-10-27 14:59:16 +0800

    simplify RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,7 +4,6 @@ namespace PHPHub\Providers;
 
 use Illuminate\Routing\Router;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Request;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -31,30 +30,27 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map(Router $router)
     {
-        // 如果访问的域名是 API_DOMAIN 配置项的，就只注册 API 路由
-        // 如果访问的域名是其他域名，则将先所有路由加上前缀 /api 后注册。
-        if (str_contains(Request::root(), config('api.domain'))) {
-            $api_router = app('Dingo\Api\Routing\Router');
-            $api_router->group([
-                'version'    => config('api.version'),
-                'prefix'     => config('api.version'),
-                'namespace'  => $this->api_controller_namespace,
-                'middleware' => 'api.throttle',
-                'limit'      => env('RATE_LIMITS'),
-                'expires'    => env('RATE_LIMITS_EXPIRES'),
-            ], function ($router) {
-                require app_path('Http/api_routes.php');
-            });
-        } else {
-            $router->group([
-                'prefix'    => 'api/'.config('api.version'),
-                'namespace' => 'PHPHub\Http\ApiControllers', ], function ($router) {
-                require app_path('Http/api_routes.php');
-            });
+        $this->configureAPIRoute();
 
-            $router->group(['namespace' => $this->controller_namespace], function ($router) {
-                require app_path('Http/routes.php');
-            });
-        }
+        $router->group(['namespace' => $this->controller_namespace], function ($router) {
+            require app_path('Http/routes.php');
+        });
+    }
+
+    /**
+     * 配置 API 路由.
+     */
+    public function configureAPIRoute()
+    {
+        $api_router = app('Dingo\Api\Routing\Router');
+        $api_router->group([
+            'version'    => env('API_PREFIX'),
+            'namespace'  => $this->api_controller_namespace,
+            'middleware' => 'api.throttle',
+            'limit'      => env('RATE_LIMITS'),
+            'expires'    => env('RATE_LIMITS_EXPIRES'),
+        ], function ($router) {
+            require app_path('Http/api_routes.php');
+        });
     }
 }
